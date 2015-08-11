a4d6de0b19dd0b2301128efa514e09c0c44c412f
eric-larkin <eal64@pitt.edu>
2015-08-11 16:41:03 -0700

    Fix merge conflict

--- app/src/main/java/com/example/larkinmcmahon/geogoals/GoalEditFragment.java
+++ app/src/main/java/com/example/larkinmcmahon/geogoals/GoalEditFragment.java
@@ -65,30 +65,7 @@ public class GoalEditFragment extends Fragment implements LoaderManager.LoaderCa
         return fragment;
     }
 
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//
-//        View rootView = inflater.inflate(R.layout.activity_goal_edit, container, false);
-//        Intent intent = getActivity().getIntent();
-////        if (intent != null && intent.hasExtra("passingGoal")) {
-////            Goal currentGoal = (Goal)intent.getSerializableExtra("passingGoal");
-////            ((TextView) rootView.findViewById(R.id.activity_goal_edit_title))
-////                    .setText(currentGoal.getTitle());
-////            int idnum = currentGoal.getOverallID();
-////            Log.d("GOALDBHELPER", String.valueOf(idnum));
-////        }
-//        if (intent != null && intent.hasExtra("dbid")) {
-//            GoalDatabaseHelper db = new GoalDatabaseHelper(getActivity().getApplicationContext());
-//            int dbid = intent.getIntExtra("dbID", 0);
-//            GoalEdit goalEdit = new GoalEdit();
-//            goalEdit.mCurrentGoal = db.getGoal(dbid);
-////            Goal currentGoal = db.getGoal(dbid);
-//            ((TextView) rootView.findViewById(R.id.activity_goal_edit_title))
-//                    .setText(goalEdit.mCurrentGoal.getTitle());
-//        }
-//        return rootView;
-//    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -112,60 +89,13 @@ public class GoalEditFragment extends Fragment implements LoaderManager.LoaderCa
     public void onActivityCreated(Bundle savedInstanceState) {
         getLoaderManager().initLoader(LOADER_ID, null, this);
         super.onActivityCreated(savedInstanceState);
-<<<<<<< HEAD
-=======
+
         mEditGoalStartDate = ((TextView) getActivity().findViewById(R.id.goal_startdate_edittext));
         mEditGoalEndDate = ((TextView) getActivity().findViewById(R.id.goal_enddate_edittext));
 
         setUpDateDialogs();
 
 
-//        Intent intent = getActivity().getIntent();
-//
-//        if (intent != null && intent.hasExtra("dbid")) {
-//            String projection[] = { GoalDatabaseHelper.KEY_GOALNAME,
-//                                    GoalDatabaseHelper.KEY_COMMENTS,
-//                                    GoalDatabaseHelper.KEY_OCCURANCES,
-//                                    GoalDatabaseHelper.KEY_TIMEFRAME};
-//            int dbid = intent.getIntExtra("dbID", 0);
-//            Cursor cursor = getActivity().getContentResolver().query(
-//                    Uri.withAppendedPath(GoalsProvider.CONTENT_URI,
-//                            String.valueOf(dbid)),projection,null,null,null);
-//            if(cursor.moveToFirst()) {
-//                String mGoalName = cursor.getString(0);
-//                String mComments = cursor.getString(1);
-//                String mOccurances = cursor.getString(2);
-//                String mTimeFrame = cursor.getString(3);
-//
-//                ((TextView) getActivity().findViewById(R.id.goal_title_editbox))
-//                        .setText(mGoalName);
-//                ((TextView) getActivity().findViewById(R.id.goal_comments_editbox))
-//                        .setText(mComments);
-//                ((TextView) getActivity().findViewById(R.id.goal_occurrences_editbox))
-//                        .setText(String.valueOf(mOccurances));
-//                ((TextView) getActivity().findViewById(R.id.goal_timeframe_editbox))
-//                        .setText(String.valueOf(mTimeFrame));
-//            }
-
-
-
-
-//            GoalDatabaseHelper db = new GoalDatabaseHelper(getActivity().getApplicationContext());
-//            int dbid = intent.getIntExtra("dbID", 0);
-//            GoalEdit goalEdit = (GoalEdit) mContext;
-//            goalEdit.mCurrentGoal = db.getGoal(dbid);
-////            Goal currentGoal = db.getGoal(dbid);
-//            ((TextView) getActivity().findViewById(R.id.goal_title_editbox))
-//                    .setText(goalEdit.mCurrentGoal.getTitle());
-//            ((TextView) getActivity().findViewById(R.id.goal_comments_editbox))
-//                    .setText(goalEdit.mCurrentGoal.getComments());
-//            ((TextView) getActivity().findViewById(R.id.goal_occurrences_editbox))
-//                    .setText(String.valueOf(goalEdit.mCurrentGoal.getOccurance()));
-//            ((TextView) getActivity().findViewById(R.id.goal_timeframe_editbox))
-//                    .setText(String.valueOf(goalEdit.mCurrentGoal.getTimeFrame()));
-
-
->>>>>>> origin/master
     }
 
     public String getTitle(){
